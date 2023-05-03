d3a0d3a2f86cd78093e81b3f967e50dc3de1f0ad
Dawid Machaj <S59073@student.ath.edu.pl>
2023-05-04 00:55:50 +0200

    Add test console project to check generic data service

---
+++ FlashcardApp.TestConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿using FlashcardApp.EntityFramework;
+using FlashcardApp.EntityFramework.Services;
+using FlashcardApp.Models;
+using FlashcardApp.Services;
+
+IDataService<Deck> deckService = new GenericDataService<Deck>(new FlashcardAppDbContextFactory());
+Console.WriteLine(deckService.Delete(1).Result);
+
+Console.ReadLine();
