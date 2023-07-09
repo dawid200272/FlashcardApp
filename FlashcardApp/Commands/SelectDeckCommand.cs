54c279f934ed7d0235af2eb245ca324471fb6637
Dawid <dawid.machaj2002@gmail.com>
2023-07-10 00:01:01 +0200

    Add command to select deck from list view

---
+++ FlashcardApp/Commands/SelectDeckCommand.cs
@@ -0,0 +1,39 @@
+﻿using FlashcardApp.Domain.Models;
+using FlashcardApp.State.Navigators;
+using FlashcardApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlashcardApp.Commands
+{
+    public class SelectDeckCommand : ICommand
+    {
+        private readonly INavigator _navigator;
+
+        public SelectDeckCommand(INavigator navigator)
+        {
+            _navigator = navigator;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is DeckViewModel deckViewModel)
+            {
+                Deck deck = deckViewModel.GetDeck();
+
+                //_navigator.CurrentViewModel = _viewModelFactory.CreateViewModel(viewType);
+            }
+        }
+    }
+}
