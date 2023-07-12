bc96dc962c6cffa9b267e19c8b2dc0a9e0c23a6e
Dawid <dawid.machaj2002@gmail.com>
2023-07-12 02:17:08 +0200

    Changes in TestConsole project

--- FlashcardApp.TestConsole/Program.cs
+++ FlashcardApp.TestConsole/Program.cs
@@ -1,14 +1,24 @@
-﻿using FlashcardApp.EntityFramework;
+﻿using FlashcardApp.Domain.Models;
+using FlashcardApp.Domain.Models.Enums;
+using FlashcardApp.Domain.Services;
+using FlashcardApp.EntityFramework;
 using FlashcardApp.EntityFramework.Services;
-using FlashcardApp.Models;
-using FlashcardApp.Services;
 
 //IDataService<Deck> deckService = new GenericDataService<Deck>(new FlashcardAppDbContextFactory());
 //Console.WriteLine(deckService.Delete(1).Result);
 
 //Console.ReadLine();
 
-List<Deck> Decks = new List<Deck>() { await Deck.CreateEmptyDeck("Default") };
 
-var cardTemplate1 = new CardTemplate();
+var cardTemplateService = new CardTemplateService();
+var deckService = new DeckService();
 
+List<Deck> Decks = new List<Deck>() { await deckService.CreateEmptyDeck("Default") };
+Decks.Add(await deckService.CreateEmptyDeck("test"));
+
+var cardTemplate1 = cardTemplateService.CreateCardTemplate(CardTemplateType.Basic, "question", "answer");
+var cardTemplate2 = cardTemplateService.CreateCardTemplate(CardTemplateType.Basic, "f", "b");
+
+
+
+Console.ReadLine();
