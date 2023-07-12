6eef516dc7274d1dc5ab0899c7598f5cd133d773
Dawid <dawid.machaj2002@gmail.com>
2023-07-12 02:25:19 +0200

    Add views and view models for add card and deck details

---
+++ FlashcardApp/ViewModels/AddCardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FlashcardApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlashcardApp.WPF.ViewModels
+{
+    public class AddCardViewModel : ViewModelBase
+    {
+        public ICommand AddCommand { get; set; }
+        public ICommand CloseCommand { get; set; }
+
+    }
+}
