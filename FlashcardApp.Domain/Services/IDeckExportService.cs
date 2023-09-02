679181d74eec13bd2b60079485f949464b66f13f
Dawid <dawid.machaj2002@gmail.com>
2023-09-02 12:04:51 +0200

    Add deck export service interface

---
+++ FlashcardApp.Domain/Services/IDeckExportService.cs
@@ -0,0 +1,17 @@
+﻿using FlashcardApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashcardApp.Domain.Services;
+public interface IDeckExportService
+{
+    /// <summary>
+    /// Export given deck
+    /// </summary>
+    /// <param name="deck">Deck to be exported</param>
+    /// <returns>Result of export</returns>
+    Task<bool> Export(Deck deck);
+}
