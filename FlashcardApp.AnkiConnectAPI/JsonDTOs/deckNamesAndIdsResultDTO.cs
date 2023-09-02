9310974d24f0ae61a5f78c7db05f17c53445445b
Dawid <dawid.machaj2002@gmail.com>
2023-09-02 12:21:16 +0200

    Add json DTOs
    Added json DTOs in Anki Connect API project.

---
+++ FlashcardApp.AnkiConnectAPI/JsonDTOs/deckNamesAndIdsResultDTO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FlashcardApp.AnkiConnectAPI.JsonDTOs;
+public class deckNamesAndIdsResultDTO
+{
+    public object? result { get; set; }
+    public string? error { get; set; }
+
+    //public class Result
+    //{
+    //    public List<DeckJsonDTO> Decks { get; set; }
+
+    //    public Result(List<DeckJsonDTO> decks)
+    //    {
+    //        Decks = decks;
+    //    }
+    //}
+
+    //public class DeckJsonDTO
+    //{
+    //    public string DeckName { get; set; }
+    //    public long Id { get; set; }
+    //}
+}
+
+public class Root
+{
+    [JsonPropertyName("models")]
+    public List<ModelJsonDTO> Models { get; } = new List<ModelJsonDTO>();
+}
+
+public class ModelJsonDTO
+{
+    [JsonPropertyName("modelName")]
+    public string ModelName { get; set; }
+
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+}
---
+++ FlashcardApp.AnkiConnectAPI/JsonDTOs/requestJsonDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FlashcardApp.AnkiConnectAPI.JsonDTOs;
+public class requestJsonDTO
+{
+    [JsonPropertyName("action")]
+    public string Action { get; set; }
+
+    [JsonPropertyName("version")]
+    public int Version { get; set; }
+}
+
+public class requestWithParamsJsonDTO
+{
+    [JsonPropertyName("action")]
+    public string Action { get; set; }
+
+    [JsonPropertyName("version")]
+    public int Version { get; set; }
+
+    [JsonPropertyName("params")]
+    public object Params { get; set; }
+}
