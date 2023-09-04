1a81452fdd09717536000465187671350ff54359
Dawid <dawid.machaj2002@gmail.com>
2023-09-04 17:03:41 +0200

    Deck export service refactor
    * Refactored deck export service interface.
    
    * Refactored deck export service implementation.

--- FlashcardApp.AnkiConnectAPI/Services/DeckExportService.cs
+++ FlashcardApp.AnkiConnectAPI/Services/DeckExportService.cs
@@ -1,6 +1,4 @@
-﻿#define TEST
-
-using FlashcardApp.AnkiConnectAPI.JsonDTOs;
+﻿using FlashcardApp.AnkiConnectAPI.JsonDTOs;
 using FlashcardApp.Domain.Models;
 using FlashcardApp.Domain.Services;
 using System;
@@ -20,251 +18,161 @@ public class DeckExportService : IDeckExportService
     private const string AnkiConnectAPIAdress = "http://localhost:8765";
     private const string TagForExportedCards = "exported-from-FlashcardApp";
 
-    public async Task<bool> Export(Deck deck)
+    public async Task<DeckExportResult> Export(Deck deck)
     {
-        using (HttpClient client = new HttpClient())
+        using HttpClient client = new HttpClient();
+
+        var endpoint = new Uri(AnkiConnectAPIAdress);
+
+        client.BaseAddress = endpoint;
+
+        #region DeckNamesAction
+
+        object deckNamesRequest = GetRequestJsonDto(actionName: "deckNames", version: AnkiConnectVersion);
+
+        DeckNamesResultJsonDto deckNamesResult = await GetResultJsonDtoAsync<DeckNamesResultJsonDto>(requestJsonDto: deckNamesRequest, requestUri: "", client);
+
+        if (deckNamesResult.Result is null)
         {
-            var endpoint = new Uri(AnkiConnectAPIAdress);
+            return new DeckExportResult(IsSuccess: false, Message: "Collection of decks cannot be fetched from Anki Connect API.");
+        }
 
-            client.BaseAddress = endpoint;
+        if (deckNamesResult.Result.Contains(deck.Name))
+        {
+            return new DeckExportResult(IsSuccess: false, Message: "Deck with given name already exists in Anki deck collection.");
+        }
 
-            #region DeckNamesAction
+        #endregion
 
-            var deckNamesRequest = new
+        #region CreateDeckAction
+
+        object createDeckParams = new
+        {
+            deck = deck.Name
+        };
+
+        object createDeckRequest = GetRequestJsonDto("createDeck", AnkiConnectVersion, createDeckParams);
+
+        CreateDeckResultJsonDto createDeckResult = await GetResultJsonDtoAsync<CreateDeckResultJsonDto>(requestJsonDto: createDeckRequest, requestUri: "", client);
+
+        if (createDeckResult.Result is null)
+        {
+            return new DeckExportResult(IsSuccess: false, Message: "Deck was not exported.");
+        }
+
+        #endregion
+
+        if (!deck.Cards.Any())
+        {
+            return new DeckExportResult(IsSuccess: true, Message: "Empty deck was exported successfully.");
+        }
+
+        #region AddNotesAction
+
+        object options = new
+        {
+            allowDuplicate = false,
+            duplicateScope = "deck"
+        };
+
+        object notesJsonDto = deck.Cards.Select(c => new
+        {
+            deckName = deck.Name,
+            modelName = AnkiConnectModelName,
+            fields = new
             {
-                action = "deckNames",
-                version = AnkiConnectVersion
-            };
-
-            string deckNamesRequestJson = JsonSerializer.Serialize(deckNamesRequest);
-
-            StringContent deckNamesContent = new StringContent(deckNamesRequestJson, Encoding.UTF8, MediaType);
-
-            var deckNamesResponse = await client.PostAsync("", deckNamesContent);
-
-            string deckNamesResponseJson = await deckNamesResponse.Content.ReadAsStringAsync();
-
-            DeckNamesResultJsonDto? deckNamesResult = JsonSerializer.Deserialize<DeckNamesResultJsonDto>(deckNamesResponseJson);
-
-            if (deckNamesResult is null ||
-                deckNamesResult.Result is null)
+                Front = c.Front,
+                Back = c.Back
+            },
+            options = options,
+            tags = new[]
             {
-                // HACK: temporary solution
-                throw new Exception("Result is null");
-            }
-
-            if (deckNamesResult.Result.Contains(deck.Name))
-            {
-                // HACK: change that to maybe ResultObject
-                throw new Exception("Deck with given name already exists in Anki deck collection");
-            }
-
-            #endregion
-
-            #region CreateDeckAction
-
-            var createDeckRequest = new
-            {
-                action = "createDeck",
-                version = AnkiConnectVersion,
-                @params = new
-                {
-                    deck = deck.Name
-                }
-            };
-
-            string createDeckRequestJson = JsonSerializer.Serialize(createDeckRequest);
-
-            StringContent createDeckContent = new StringContent(createDeckRequestJson, Encoding.UTF8, MediaType);
-
-            var createDeckResponse = await client.PostAsync("", createDeckContent);
-            //var createDeckResponse = await client.PostAsJsonAsync("", createDeckRequest);
-
-            string createDeckResultJson = await createDeckResponse.Content.ReadAsStringAsync();
-
-            CreateDeckResultJsonDto? createDeckResult = JsonSerializer.Deserialize<CreateDeckResultJsonDto>(createDeckResultJson);
-
-            if (createDeckResult is null ||
-                createDeckResult.Result is null)
-            {
-                throw new Exception("Deck was not created");
-            }
-
-            #endregion
-
-            if (!deck.Cards.Any())
-            {
-                return true;
-            }
-
-            #region AddNotesAction
-
-            var options = new
-            {
-                allowDuplicate = false,
-                duplicateScope = "deck"
-            };
-
-            var notesJsonDto = deck.Cards.Select(c => new
-            {
-                deckName = deck.Name,
-                modelName = AnkiConnectModelName,
-                fields = new
-                {
-                    Front = c.Front,
-                    Back = c.Back
-                },
-                options = options,
-                tags = new[]
-                {
                     TagForExportedCards
                 }
-            });
+        });
 
-            var addNotesRequest = new
-            {
-                action = "addNotes",
-                version = AnkiConnectVersion,
-                @params = new
-                {
-                    notes = notesJsonDto
-                }
-            };
+        object addNotesParams = new
+        {
+            notes = notesJsonDto
+        };
 
-            var addNotesRequestJson = JsonSerializer.Serialize(addNotesRequest);
+        object addNotesRequest = GetRequestJsonDto("addNotes", AnkiConnectVersion, addNotesParams);
 
-            StringContent addNotesContent = new StringContent(addNotesRequestJson, Encoding.UTF8, MediaType);
+        AddNotesResultJsonDto addNotesResult = await GetResultJsonDtoAsync<AddNotesResultJsonDto>(requestJsonDto: addNotesRequest, requestUri: "", client);
 
-            var addNotesResponse = await client.PostAsync("", addNotesContent);
-            //var addNotesResponse = await client.PostAsJsonAsync("", addNotesRequest);
-
-            string addNotesResultJson = await addNotesResponse.Content.ReadAsStringAsync();
-
-            AddNotesResultJsonDto? addNotesResult = JsonSerializer.Deserialize<AddNotesResultJsonDto>(addNotesResultJson);
-
-            if (addNotesResult is null ||
-                addNotesResult.Result is null)
-            {
-                throw new Exception("Export of deck cards was not successful");
-            }
-
-            #endregion
-
-            // TODO: Change return type to some ResultObject
-            return true;
-
-//            requestJsonDto requestJsonDTO = new requestJsonDto()
-//            {
-//                Action = "modelNamesAndIds",
-//                Version = AnkiConnectVersion
-//            };
-
-//            requestWithParamsJsonDto requestWithParamsJsonDTO = new requestWithParamsJsonDto()
-//            {
-//                Action = "modelFieldNames",
-//                Version = AnkiConnectVersion,
-//                Params = new
-//                {
-//                    modelName = AnkiConnectModelName
-//                }
-//            };
-
-//            //var deckNamesRequest = new
-//            //{
-//            //    action = "modelFieldNames",
-//            //    version = ankiConnectVersion,
-//            //    @params = new
-//            //    {
-//            //        modelName = AnkiConnectModelName
-//            //    }
-//            //};
-
-//#if TEST
-//            CardTemplate cardTemplate = new CardTemplate("front 1 content", "back 1 content");
-
-//            var model = cardTemplate.TemplateType.ToString();
-
-//            Deck deck1 = new Deck("deck");
-
-//            Card card = new Card(cardTemplate, deck1);
-
-//            deck1.AddCard(card);
-//            deck1.AddCard(card);
-//            deck1.AddCard(card);
-//            deck1.AddCard(card);
-//#endif
-
-//            var notes = deck1.Cards.Select(c => new
-//            {
-//                deckName = deck.Name,
-//                modelName = AnkiConnectModelName,
-//                fields = new
-//                {
-//                    Front = c.Front,
-//                    Back = c.Back
-//                },
-//                tags = new[]
-//                {
-//                    TagForExportedCards
-//                }
-//            });
-
-//            var request = new
-//            {
-//                action = "addNotes",
-//                version = AnkiConnectVersion,
-//                @params = new
-//                {
-//                    notes = notes
-//                }
-//            };
-
-//            var jsonRequest = JsonSerializer.Serialize(deckNamesRequest);
-
-//            StringContent content = new StringContent(jsonRequest, Encoding.UTF8, MediaType);
-
-//            var response = await client.PostAsync(endpoint, content);
-
-//            var jsonResponse = await response.Content.ReadAsStringAsync();
-
-//            //deckNamesResultJsonDto? responseResultDTO = JsonSerializer.Deserialize<deckNamesResultJsonDto>(jsonResponse);
-
-//            AddNotesResultJsonDto? responseResultDTO = JsonSerializer.Deserialize<AddNotesResultJsonDto>(jsonResponse);
-
-//            var error = responseResultDTO?.Error;
-
-//            if (error is not null)
-//            {
-//                throw new Exception(error);
-//            }
-
-//            object? result = responseResultDTO?.Result;
-
-//            if (result is null)
-//            {
-//                return false;
-//            }
-
-//            string? json = result.ToString();
-
-//            if (string.IsNullOrWhiteSpace(json))
-//            {
-//                return false;
-//            }
-
-//            if (deckNamesRequest.action == "modelNamesAndIds")
-//            {
-//                List<ModelJsonDto> objects = GetModelJsonDTOList(json);
-//            }
-
-//            if (deckNamesRequest.action == "deckNamesAndIds")
-//            {
-//                var objects = GetDeckJsonDTOList(json);
-//            }
-
-//            return true;
+        if (addNotesResult.Result is null)
+        {
+            return new DeckExportResult(IsSuccess: false, Message: "Export of deck cards was not successful.");
         }
+
+        #endregion
+
+        return new DeckExportResult(IsSuccess: true, Message: "Deck was exported successfully.");
     }
+
+    #region Helper Methods
+
+    /// <summary>
+    /// Get request Json dto for given arguments.
+    /// </summary>
+    /// <param name="actionName">Name of Anki Connect API action.</param>
+    /// <param name="version">Version of Anki Connect API.</param>
+    /// <param name="params">Params object for Anki Connect API request.</param>
+    /// <returns>Request Json dto without params if params is <see langword="null"/> and with params otherwise.</returns>
+    private object GetRequestJsonDto(string actionName, int version, object? @params = null)
+    {
+        if (@params is null)
+        {
+            return new
+            {
+                action = actionName,
+                version = version
+            };
+        }
+
+        return new
+        {
+            action = actionName,
+            version = version,
+            @params = @params
+        };
+    }
+
+    // TODO: Document this
+    private async Task<T> GetResultJsonDtoAsync<T>(object requestJsonDto, string? requestUri, HttpClient client)
+        where T : class, new()
+    {
+        StringContent requestContent = GetRequestContent(requestJsonDto);
+
+        var response = await client.PostAsync("", requestContent);
+
+        T resultJsonDto = await GetResultJsonDto<T>(response: response);
+
+        return resultJsonDto;
+    }
+
+    // TODO: Document this
+    private StringContent GetRequestContent(object requestJsonDto)
+    {
+        string requestJson = JsonSerializer.Serialize(requestJsonDto);
+
+        StringContent requestContent = new StringContent(requestJson, Encoding.UTF8, MediaType);
+
+        return requestContent;
+    }
+
+    // TODO: Document this
+    private async Task<T> GetResultJsonDto<T>(HttpResponseMessage response)
+        where T : class, new()
+    {
+        string responseJson = await response.Content.ReadAsStringAsync();
+
+        T? resultJsonDto = JsonSerializer.Deserialize<T>(responseJson);
+
+        return resultJsonDto ?? new T();
+    }
+
+    #endregion
 
     /// <summary>
     /// Get list of <see cref="ModelJsonDto"/>s
--- FlashcardApp.Domain/Services/IDeckExportService.cs
+++ FlashcardApp.Domain/Services/IDeckExportService.cs
@@ -9,9 +9,16 @@ namespace FlashcardApp.Domain.Services;
 public interface IDeckExportService
 {
     /// <summary>
-    /// Export given deck
+    /// Export given <paramref name="deck"/>.
     /// </summary>
-    /// <param name="deck">Deck to be exported</param>
-    /// <returns>Result of export</returns>
-    Task<bool> Export(Deck deck);
+    /// <param name="deck"><see cref="Deck"/> to be exported.</param>
+    /// <returns><see cref="DeckExportResult"/></returns>
+    Task<DeckExportResult> Export(Deck deck);
 }
+
+/// <summary>
+/// Result object for <see cref="IDeckExportService"/>.
+/// </summary>
+/// <param name="IsSuccess">It is <see langword="true"/> when deck was exported successfully; otherwise it is <see langword="false"/>.</param>
+/// <param name="Message">When <see cref="IsSuccess"/> is <see langword="true"/> it is success message; otherwise it is error message.</param>
+public record DeckExportResult(bool IsSuccess, string Message);
